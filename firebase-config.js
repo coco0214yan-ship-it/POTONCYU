5fe43bf2aadf6c0ed19b2d5f89b0e6742a1099cd
Potonchu Publisher <potonchu@example.invalid>
2026-07-02 16:46:34 +0900

    Configure Firebase for potonchu

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 // Firebaseコンソールで表示されるWebアプリ設定をここに貼ります。
 // 空のままなら、同じPCの別タブだけで試せるデモ同期になります。
 window.POTONCHU_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDAm4g9-zTrAFqXvUY9Pi_AWCmzNZR-tPM",
+  authDomain: "potonchu.firebaseapp.com",
+  projectId: "potonchu",
+  storageBucket: "potonchu.firebasestorage.app",
+  messagingSenderId: "639859878416",
+  appId: "1:639859878416:web:962a289acb8742bb567895",
 };
